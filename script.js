aa2b583cf73489b85628e7497a4db8a5dfcf4917
sanogod843-beep <sanogod843@gmail.com>
2026-07-25 17:56:50 +0000

    Add initial script.js with console log and alert

---
+++ script.js
@@ -0,0 +1,8 @@
+script.js
+// js/script.js
+console.log("Le script est chargé !");
+
+// Test animation au clic
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Bienvenue sur mon site !");
+});
